43d98955c43f3ac6f4843d542a321b7c05fb5fee
Samson50 <jakeweiss50@gmail.com>
2017-05-26 12:35:45 -0400

    working as expected with benchmarking removed but results documented in code

--- src/mainWhiteOnly.cpp
+++ src/mainWhiteOnly.cpp
@@ -83,7 +83,7 @@ int main(int argc, char** argv) {
     std::string point_cloud_frame_id = "/zed_current_frame";
     ros::Publisher white_publisher_cloud = nh.advertise<sensor_msgs::PointCloud2> (white_cloud_topic, 2);
     
-    ROS_INFO("PUB IMAGES IS: %d",pub_images);
+    ROS_INFO("JETSON TK1 pub_image value is (0 means do not publish images): %d",pub_images);
     image_transport::Publisher image_publisher;
     image_transport::Publisher white_publisher;
     if (pub_images){
@@ -95,36 +95,37 @@ int main(int argc, char** argv) {
     ColorFilter color_filter;
 
     ROS_INFO("JETSON TK1 is entering main loop\n");
-    log2file("top,afgrab,afRmeasure,afRImage,afFilter,afDwnImg,b4Forloop,afForloop,b4Sleep, CLOCKS_PER_SEC,cpu_cloud.rows,cpu_cloud.cols,cpu_cloud.step0,cpu_cloud.step1,cpu_cloud.elemSize, cpu_cloud.elemSize1,cpu_cloud.depth,cpu_cloud.channels,sizeof\n"); 
-    ros::Rate loop_rate(30);
+    // The benchmark timing is based on git commit: 358bcfa4d3bc14d0e72ee38c2ca35b9e420016f6 master branch
+    //log2file("top,afgrab,afRmeasure,afRImage,afFilter,afDwnImg,b4Forloop,afForloop,b4Sleep, CLOCKS_PER_SEC,cpu_cloud.rows,cpu_cloud.cols,cpu_cloud.step0,cpu_cloud.step1,cpu_cloud.elemSize, cpu_cloud.elemSize1,cpu_cloud.depth,cpu_cloud.channels,sizeof\n"); 
+    ros::Rate loop_rate(20);
     while (nh.ok()) {
         std::ostringstream outStream;
         std::ostringstream sizesLog;
-	    outStream << clock() << ","; // TIMER: top
+	    //outStream << clock() << ","; // TIMER: top
         pcl::PointXYZRGBA point;
         pcl::PointCloud<pcl::PointXYZRGBA> white_cloud;
         white_cloud.clear();
         cv::Mat cloud_image;
 	    
         if (!zed->grab(dm_type)) { // grabs a new image
-	        outStream << clock() << ","; // TIMER: afgrab
+	        //outStream << clock() << ","; // TIMER: afgrab 20.16ms/47% of time
 		    //Get image from ZED using the gpu buffer
 		    cv::Mat cpu_cloud = slMat2cvMat(zed->retrieveMeasure(MEASURE::XYZBGRA));
-		    sizesLog << cpu_cloud.rows    << "," << cpu_cloud.cols       << ",";
-		    sizesLog << cpu_cloud.step[0]    << ","<< cpu_cloud.step[1]    << "," << cpu_cloud.elemSize() << "," << cpu_cloud.elemSize1() << ",";
-		    sizesLog << cpu_cloud.depth() << "," << cpu_cloud.channels() << "," << sizeof(cpu_cloud)<< ",";
-	        outStream << clock() << ","; // TIMER: afRmeasure 
+		    //sizesLog << cpu_cloud.rows    << "," << cpu_cloud.cols       << ",";
+		    //sizesLog << cpu_cloud.step[0]    << ","<< cpu_cloud.step[1]    << "," << cpu_cloud.elemSize() << "," << cpu_cloud.elemSize1() << ",";
+		    //sizesLog << cpu_cloud.depth() << "," << cpu_cloud.channels() << "," << sizeof(cpu_cloud)<< ",";
+	        //outStream << clock() << ","; // TIMER: afRmeasure 6ms/13.8% of time
             cv::Mat  host_image = slMat2cvMat(zed->retrieveImage(sl::zed::SIDE::LEFT));
-	        outStream << clock() << ","; // TIMER: afRImage
+	        //outStream << clock() << ","; // TIMER: afRImage 2.6ms/6.1% of time
 		
 		    //Filter the image for white lines
 		    cv::gpu::GpuMat cv_filteredImage = color_filter.findLines(host_image);
-	        outStream << clock() << ","; // TIMER: afFilter
+	        //outStream << clock() << ","; // TIMER: afFilter 21.1/9.1% of time
 		    cv_filteredImage.download(cloud_image); //move image to cpu memory
-	        outStream << clock() << ","; // TIMER: afDwnImg
-		    sizesLog << cloud_image.rows    << "," << cloud_image.cols       << ",";
-		    sizesLog << cloud_image.step    << "," << cloud_image.elemSize() << "," << cloud_image.elemSize1() << ",";
-		    sizesLog << cloud_image.depth() << "," << cloud_image.channels() << "," << sizeof(cloud_image)<< "\n";
+	        //outStream << clock() << ","; // TIMER: afDwnImg 0.65ms/1.6% of time
+		    //sizesLog << cloud_image.rows    << "," << cloud_image.cols       << ",";
+		    //sizesLog << cloud_image.step    << "," << cloud_image.elemSize() << "," << cloud_image.elemSize1() << ",";
+		    //sizesLog << cloud_image.depth() << "," << cloud_image.channels() << "," << sizeof(cloud_image)<< "\n";
 		    
 		    if(pub_images){
 	  	        sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(std_msgs::Header(), "bgra8", host_image).toImageMsg();
@@ -133,10 +134,12 @@ int main(int argc, char** argv) {
                 white_publisher.publish(fltrd_msg);
             }
 		    //imwrite("/home/ubuntu/image.jpg", cloud_image);
-	        outStream << clock() << ","; // TIMER: b4Forloop		
+	        //outStream << clock() << ","; // TIMER: b4Forloop 3.1ms/7.2% of time 		
         	//Iterate through points in cloud
 
-		    for (int i = 0; i < cloud_image.rows; i++) {
+            //TODO This could be faster if we used the GPU to produce a list of pixels of interest and iterated over just those pixels as opposed to
+            // iterating over every pixel in the image.
+		    for (int i = 0; i < cloud_image.rows; i++) { 
 		        for (int j = 0; j < cloud_image.cols;j++) {		        
                     int wl_point = cloud_image.at<int>(i,j);
 		            if (wl_point > 10){
@@ -149,7 +152,7 @@ int main(int argc, char** argv) {
 		            }
 	            }
 	        }		    
-	        outStream << clock() << ","; // TIMER: afForloop		
+	        //outStream << clock() << ","; // TIMER: afForloop 0.91ms/2.2% of time		
 
 		    if (white_cloud.height == 0){
 			    white_cloud.push_back(pcl::PointXYZRGBA());
@@ -163,13 +166,14 @@ int main(int argc, char** argv) {
        		white_publisher_cloud.publish(output_white);
 		    white_cloud.clear();
 		    //Spin once updates dynamic_reconfigure values
-	        outStream << clock() << ","<< CLOCKS_PER_SEC << ","; // TIMER: b4Sleep	
-            log2file(outStream.str());
-            log2file(sizesLog.str());
+	        //outStream << clock() << ","<< CLOCKS_PER_SEC << ","; // TIMER: b4Sleep 0.29ms//0.7% of time	
+            //log2file(outStream.str());
+            //log2file(sizesLog.str());
 		    ros::spinOnce();
-		    loop_rate.sleep();
+		    loop_rate.sleep(); 
 	    }
-   }
+   } // Average time for the big while loop is: 43.1 ms about 23hz b4 change rate from 30hz to 20hz.
+   // At full speed (5mph)the robot moves about 11cm in the time it takes to complete this loop.
     delete zed;
     return 0;
 }
